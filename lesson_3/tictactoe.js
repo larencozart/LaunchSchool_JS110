93c0242c41ef65af543c14fd28c79c93f3487ca1
larebear007 <larencozart@gmail.com>
2023-11-12 09:08:22 -0600

    Add offensive functionality and ability for player to choose who moves first

--- lesson_3/tictactoe.js
+++ lesson_3/tictactoe.js
@@ -6,8 +6,10 @@ const rls = require('readline-sync');
 const INITIAL_MARKER = ' ';
 const PLAYER_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
+const MIDDLE_BOARD_SQUARE = '5';
 const WINNING_COMBOS = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
   [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -71,7 +73,34 @@ function playerChoosesSquare(board) {
 }
 
 // eslint-disable-next-line max-lines-per-function
-function isDefensiveMovePresent(board) {
+function playOffense(board) {
+  let square = null;
+
+  for (let combo = 0; combo < WINNING_COMBOS.length; combo += 1) {
+    let [ sq1, sq2, sq3 ] = WINNING_COMBOS[combo];
+    if (board[sq1] === COMPUTER_MARKER
+        && board[sq2] === COMPUTER_MARKER
+        && board[sq3] === INITIAL_MARKER) {
+      square = sq3;
+      break;
+    } else if (board[sq1] === COMPUTER_MARKER
+               && board[sq3] === COMPUTER_MARKER
+               && board[sq2] === INITIAL_MARKER) {
+      square = sq2;
+      break;
+    } else if (board[sq2] === COMPUTER_MARKER
+               && board[sq3] === COMPUTER_MARKER
+               && board[sq1] === INITIAL_MARKER) {
+      square = sq1;
+      break;
+    }
+  }
+
+  return square;
+}
+
+// eslint-disable-next-line max-lines-per-function
+function playDefense(board) {
   let square = null;
 
   for (let combo = 0; combo < WINNING_COMBOS.length; combo += 1) {
@@ -99,10 +128,15 @@ function isDefensiveMovePresent(board) {
 
 function computerChoosesSquare(board) {
   let square;
+  const defensiveMove = playDefense(board);
+  const offensiveMove = playOffense(board);
 
-  let defensiveMove = isDefensiveMovePresent(board);
-  if (defensiveMove) {
+  if (offensiveMove) {
+    square = offensiveMove;
+  } else if (defensiveMove) {
     square = defensiveMove;
+  } else if (emptySquares(board).includes(MIDDLE_BOARD_SQUARE)) {
+    square = MIDDLE_BOARD_SQUARE;
   } else {
     let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
     square = emptySquares(board)[randomIndex];
@@ -156,30 +190,37 @@ function displayGameResult(board) {
 }
 
 // currently displaying and returning the winner? should i change this?
-function playSingleGame(scoreObj) {
+// eslint-disable-next-line max-statements, max-lines-per-function
+function playSingleGame(firstToMove, scoreObj) {
   let board = initializeBoard();
 
   while (true) {
     console.clear();
-
     if (scoreObj) {
       displayCurrentScore(scoreObj);
     }
-
     displayBoard(board);
 
-    playerChoosesSquare(board);
-    if (isGameWon(board) || isBoardFull(board)) break;
+    if (firstToMove === 'player') {
+      playerChoosesSquare(board);
+      if (isGameWon(board) || isBoardFull(board)) break;
+      computerChoosesSquare(board);
+      if (isGameWon(board) || isBoardFull(board)) break;
 
-    computerChoosesSquare(board);
-    if (isGameWon(board) || isBoardFull(board)) break;
+    } else {
+      computerChoosesSquare(board);
+      if (isGameWon(board) || isBoardFull(board)) break;
 
+      displayBoard(board);
+
+      playerChoosesSquare(board);
+      if (isGameWon(board) || isBoardFull(board)) break;
+    }
   }
 
   console.clear();
   displayBoard(board);
   displayGameResult(board);
-
   return findWinner(board);
 }
 
@@ -208,13 +249,13 @@ function displayMatchResult(winner) {
   }
 }
 
-function playMatchGame() {
+function playMatchGame(firstToMove) {
   const MATCH_WINNING_SCORE = 5;
   let scores = { player : 0, computer : 0};
 
   while (true) {
-    let currentWinner = playSingleGame(scores);
-    scores[currentWinner] += 1; // updates score obj-> so small, decided not to create helper function
+    let currentWinner = playSingleGame(firstToMove, scores);
+    scores[currentWinner] += 1;
 
     if (isMatchWon(scores, MATCH_WINNING_SCORE)) break;
 
@@ -235,6 +276,17 @@ function playAgain() {
   }
 
   return response === 'y';
+}
+
+function playerChoosesWhoGoesFirst() {
+  prompt('Who do you want to make the first move? Enter (1) You, or (2) The Computer: ');
+  let response = rls.question().trim();
+  while (!['1', '2'].includes(response)) {
+    prompt('Please only enter (1) to allow you to make the first move or (2) to allow the computer to make the first move: ');
+    response = rls.question().trim();
+  }
+
+  return response === '1' ? 'player' : 'computer';
 }
 
 function playerChoosesGameStyle() {
@@ -267,15 +319,17 @@ function runGameEngine() {
   while (true) {
     console.clear();
     prompt("Welcome to Tic-Tac-Toe!");
-    let chosenGameStyle = playerChoosesGameStyle();
 
-    displayRules(chosenGameStyle);
+    const GAME_STYLE = playerChoosesGameStyle();
+    const FIRST_TO_MOVE = playerChoosesWhoGoesFirst();
 
-    // could extract if statement to separate function
-    if (chosenGameStyle === 's') {
-      playSingleGame();
+    displayRules(GAME_STYLE);
+
+    // could extract if statement to separate function 'playChosenGameStyle'
+    if (GAME_STYLE === 's') {
+      playSingleGame(FIRST_TO_MOVE);
     } else {
-      playMatchGame();
+      playMatchGame(FIRST_TO_MOVE);
     }
 
     if (playAgain() === false) break;
